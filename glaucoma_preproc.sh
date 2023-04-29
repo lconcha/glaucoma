364a1089e3061384ac78ac51322848e34bcf4b29
Luis Concha <lconcha@gmail.com>
2023-04-28 18:36:03 -0600

    added readout time

--- glaucoma_preproc.sh
+++ glaucoma_preproc.sh
@@ -4,22 +4,24 @@ source `which my_do_cmd`
 sID=$1
 
 
+bids_dir=/misc/mansfield/lconcha/exp/glaucoma/bids
 nifti_dir=/misc/mansfield/lconcha/exp/glaucoma/raw
-preproc_dir=/misc/mansfield/lconcha/exp/glaucoma/preproc
+#preproc_dir=/misc/mansfield/lconcha/exp/glaucoma/preproc
 
-DWI_HB_full=$(find $nifti_dir/${sID} -name ${sID}_*_DWI_HB.nii.gz)
-DWI_HB_pepolar=$(find $nifti_dir/${sID} -name ${sID}_*_DWI_HB_pepolar.nii.gz)
-slspec=${DWI_HB_full%.nii.gz}_slspec.txt
-HB_bvec=${DWI_HB_full%.nii.gz}.bvec
-HB_bval=${DWI_HB_full%.nii.gz}.bval
-DWI_MUSE_full=$(find $nifti_dir/${sID} -name ${sID}_*_DWI-MUSE.nii.gz)
-DWI_MUSE_pepolar=$(find $nifti_dir/${sID} -name ${sID}_*_DWI-MUSE_pepolar.nii.gz)
+DWI_HB_full=${bids_dir}/sub-${sID}/dwi/sub-${sID}_acq-hb_dwi.nii.gz
+DWI_HB_pepolar=${bids_dir}/sub-${sID}/fmap/sub-${sID}_acq-hb_epi.nii.gz
+DWI_MUSE_full=${bids_dir}/sub-${sID}/dwi/sub-${sID}_acq-muse_dwi.nii.gz
+DWI_MUSE_pepolar=${bids_dir}/sub-${sID}/fmap/sub-${sID}_acq-muse_epi.nii.gz
+
+#slspec=${DWI_HB_full%.nii.gz}_slspec.txt
+#HB_bvec=${DWI_HB_full%.nii.gz}.bvec
+#HB_bval=${DWI_HB_full%.nii.gz}.bval
 
 
 
 
 isOK=1
-for f in $DWI_HB_full $DWI_HB_pepolar $slspec $HB_bvec $HB_bval
+for f in $DWI_HB_full $DWI_HB_pepolar $DWI_MUSE_full $DWI_MUSE_pepolar
 do
   if [ ! -f $f ]; then echolor red "[ERROR] File does not exist: $f"; isOK=0;
   else echolor white "   found: $f";fi
@@ -27,32 +29,56 @@ done
 if [ $isOK -eq 0 ]; then echolor red "[ERROR] Cannot continue."; exit 2;fi
 
 
-mkdir -p $preproc_dir/${sID}
+mkdir -p $bids_dir/derivatives/sub-${sID}/{dwi,anat}
+
+
 tmpDir=$(mktemp -d)
 
-my_do_cmd dwidenoise \
-  ${DWI_HB_full} $preproc_dir/${sID}/dwi_HB_d.mif
+my_do_cmd mrconvert \
+  -json_import ${DWI_HB_full%.nii.gz}.json \
+  -fslgrad ${DWI_HB_full%.nii.gz}.{bvec,bval} \
+  $DWI_HB_full \
+  ${tmpDir}/dwi_hb.mif
+
+
+fcheck=$bids_dir/derivatives/sub-${sID}/dwi/sub-${sID}_acq-hb_dwi_d.nii.gz
+if [ ! -f $fcheck ]; then
+  my_do_cmd dwidenoise \
+    ${tmpDir}/dwi_hb.mif ${tmpDir}/dwi_hb_d.mif
+  my_do_cmd mrconvert \
+    -export_grad_fsl $bids_dir/derivatives/sub-${sID}/dwi/sub-${sID}_acq-hb_dwi_d.{bvec,bval} \
+    -json_export $bids_dir/derivatives/sub-${sID}/dwi/sub-${sID}_acq-hb_dwi_d.json \
+    ${tmpDir}/dwi_hb_d.mif \
+    $bids_dir/derivatives/sub-${sID}/dwi/sub-${sID}_acq-hb_dwi_d.nii.gz
+else echolor green "[INFO] File exists: $fcheck"; fi
+
+
 
 
 mrconvert -coord 3 0 ${DWI_HB_full}    ${tmpDir}/HB_b0_PA.nii
 mrconvert -coord 3 0 ${DWI_HB_pepolar} ${tmpDir}/HB_b0_AP.nii
 mrcat -axis 3 ${tmpDir}/HB_b0_PA.nii ${tmpDir}/HB_b0_AP.nii ${tmpDir}/HB_b0_pair.nii
-mrinfo ${tmpDir}/HB_b0_pair.nii
 
+
+readout_time=$(jq -r .TotalReadoutTime $bids_dir/derivatives/sub-${sID}/dwi/sub-${sID}_acq-hb_dwi_d.json) 
 
 dwifslpreproc \
-  -fslgrad $HB_bvec $HB_bval \
-  $preproc_dir/${sID}/dwi_HB_d.mif \
-  $preproc_dir/${sID}/dwi_HB_de.mif \
-  -rpe_pair \
-  -se_epi ${tmpDir}/HB_b0_pair.nii \
+  -fslgrad $bids_dir/derivatives/sub-${sID}/dwi/sub-${sID}_acq-hb_dwi_d.{bvec,bval} \
+  $bids_dir/derivatives/sub-${sID}/dwi/sub-${sID}_acq-hb_dwi_d.nii.gz \
+  $bids_dir/derivatives/sub-${sID}/dwi/sub-${sID}_acq-hb_dwi_de.nii.gz \
   -pe_dir pa \
+  -rpe_pair \
+  -readout_time $readout_time \
+  -se_epi ${tmpDir}/HB_b0_pair.nii \
   -align_seepi \
   -eddy_options "  --data_is_shelled --slm=linear" \
-  -eddy_slspec $slspec \
   -scratch ${tmpDir} \
-  -eddyqc_text $preproc_dir/${sID}
+  -eddyqc_text $bids_dir/derivatives/sub-${sID}/dwi
 
+#-eddy_slspec $slspec \
+
+# -json_import $bids_dir/derivatives/sub-${sID}/dwi/sub-${sID}_acq-hb_dwi_d.json \
+    
 
 
   rm -fR $tmpDir
