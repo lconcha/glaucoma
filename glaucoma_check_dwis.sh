6dbb1d4a9dda0cf02cbfd32f704bd540fb26b139
Luis Concha <lconcha@gmail.com>
2023-06-13 19:29:03 -0600

    first

---
+++ glaucoma_check_dwis.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+bids_dir=/misc/mansfield/lconcha/exp/glaucoma/bids
+
+s=$1
+
+isOK=1
+sID=$(echo $s | awk -F- '{print $NF}')
+echo ""
+echolor cyan "[INFO] Checking files for sub-${sID}"
+
+
+HB=${bids_dir}/sub-${sID}/dwi/sub-${sID}_acq-hb_dwi.nii.gz
+HB_pepolar=${bids_dir}/sub-${sID}/fmap/sub-${sID}_acq-hb_epi.nii.gz
+HB_de=${bids_dir}/derivatives/sub-${sID}/dwi/sub-${sID}_acq-hb_dwi_de.mif
+
+
+MUSE=${bids_dir}/sub-${sID}/dwi/sub-${sID}_acq-muse_dwi.nii.gz
+MUSE_pepolar=${bids_dir}/sub-${sID}/fmap/sub-${sID}_acq-muse_epi.nii.gz
+MUSE_de=${bids_dir}/derivatives/sub-${sID}/dwi/sub-${sID}_acq-muse_dwi_de.mif
+for f in $HB $HB_pepolar $HB_de  $MUSE $MUSE_pepolar $MUSE_de 
+do
+if [ ! -f $f ]
+then
+    echolor orange "[WARN] Missing file: $f"
+else
+    echolor yellow "[INFO] Found $f"
+    sz=$(mrinfo -size $f)
+    echolor yellow "       Dimensions: $sz"
+fi
+done
+
