2a168a2d1cc1ae419b1ae6d19ea39a542d49ca27
Luis Concha <lconcha@gmail.com>
2023-11-24 12:19:10 -0600

    do not convert subjects already converted

--- glaucoma_convert.sh
+++ glaucoma_convert.sh
@@ -17,6 +17,17 @@ config=/misc/mansfield/lconcha/software/glaucoma/glaucoma_dcm2bids_config.json
 dcm=$(find $dicom_dir -name IM*0001.dcm | head -n 1)
 sID=$(dcminfo -tag 0010 0020 "$dcm" | awk '{print $2}')
 
+echo $sID
+
+fcheck=${bids_dir}/sub-${sID}/anat/sub-${sID}_T1w.nii.gz
+if [ -f $fcheck ]
+then
+  echolor green "[INFO] sub-${sID} has already been converted"
+  echolor green "[INFO] If you want to re-convert, delete the following folder first: ${bids_dir}/sub-${sID}"
+  exit 0
+fi
+
+
 
 dcm2bids -d $dicom_dir \
          -p $sID \
